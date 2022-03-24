3626388259c7b530cefa227495a4ceff13b763fc
AhmedRashad <ahmedrabie441@gmail.com>
2022-03-24 05:16:39 +0200

    serve frontend

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const colers = require("colors");
 const dotenv = require("dotenv").config();
@@ -11,6 +12,16 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+
+//serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+}
 
 app.use(errorHandler);
 
